9c05b05332f3c0240725ebde4a857be6a0ff0b19
Francois-David Lessard <francois-david.lessard@nbc.ca>
2021-03-23 16:26:20 -0400

    first commit

---
+++ src/main/java/io/fdlessard/codebites/batch/Customer.java
@@ -0,0 +1,32 @@
+package io.fdlessard.codebites.batch;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Table(name = "customer", schema = "public")
+@Entity
+@SuperBuilder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer extends BaseEntity {
+
+  @NotBlank(message = "lastName name cannot be blank")
+  @Size(min = 2, message = "lastName must have more thant 2 characters")
+  private String lastName;
+
+  @NotBlank(message = "firstName name cannot be blank")
+  @Size(min = 2, message = "firstName must have more thant 2 characters")
+  private String firstName;
+
+  @NotBlank(message = "company name cannot be blank")
+  @Size(min = 2, message = "company must have more thant 2 characters")
+  private String company;
+
+}
